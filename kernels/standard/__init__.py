36cea870e349bd55bc635769d413f8246cb56708
Vahid Kazemi <vkazemi@gmail.com>
2017-10-28 22:29:52 -0700

    First commit.

---
+++ kernels/standard/__init__.py
@@ -0,0 +1,17 @@
+import cast
+import constants
+import control
+import datasets
+import logic
+import losses
+import math
+import metrics
+import nn
+import nonlinearity
+import optimization
+import random
+import reduction
+import shape
+import summary
+import variables
+
